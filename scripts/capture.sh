80178e1745f5c21a3bba6f65614ef45af18a5388
Yuxiang Yang <yxyang@ubuntu.(none)>
2016-04-14 23:09:35 -0700

    PR2 data captured

---
+++ scripts/capture.sh
@@ -0,0 +1,19 @@
+cd ~/pr2_pic_capture/img/cam1 && rosrun image_view extract_images image:=/cam1/image_raw &
+cd ~/pr2_pic_capture/img/cam2 && rosrun image_view extract_images image:=/cam2/image_raw &
+cd ~/pr2_pic_capture/img/cam3 && rosrun image_view extract_images image:=/cam3/image_raw &
+cd ~/pr2_pic_capture/img/cam4 && rosrun image_view extract_images image:=/cam4/image_raw &
+cd ~/pr2_pic_capture/img/cam5 && rosrun image_view extract_images image:=/cam5/image_raw &
+cd ~/pr2_pic_capture/img/cam6 && rosrun image_view extract_images image:=/cam6/image_raw &
+cd ~/pr2_pic_capture/img/cam7 && rosrun image_view extract_images image:=/cam7/image_raw &
+cd ~/pr2_pic_capture/img/cam8 && rosrun image_view extract_images image:=/cam8/image_raw &
+cd ~/pr2_pic_capture/img/cam9 && rosrun image_view extract_images image:=/cam9/image_raw &
+cd ~/pr2_pic_capture/img/cam10 && rosrun image_view extract_images image:=/cam10/image_raw &
+cd ~/pr2_pic_capture/img/cam11 && rosrun image_view extract_images image:=/cam11/image_raw &
+cd ~/pr2_pic_capture/img/cam12 && rosrun image_view extract_images image:=/cam12/image_raw &
+cd ~/pr2_pic_capture/img/cam13 && rosrun image_view extract_images image:=/cam13/image_raw &
+cd ~/pr2_pic_capture/img/cam14 && rosrun image_view extract_images image:=/cam14/image_raw &
+cd ~/pr2_pic_capture/img/cam15 && rosrun image_view extract_images image:=/cam15/image_raw &
+cd ~/pr2_pic_capture/img/cam16 && rosrun image_view extract_images image:=/cam16/image_raw &
+cd ~/pr2_pic_capture/img/cam17 && rosrun image_view extract_images image:=/cam17/image_raw &
+cd ~/pr2_pic_capture/img/cam18 && rosrun image_view extract_images image:=/cam18/image_raw &
+wait
